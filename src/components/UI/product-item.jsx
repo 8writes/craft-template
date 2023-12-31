100b47a27b4d4dfe4a0a0f8e414292d22c2b47eb
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-12-31 17:56:05 +0100

    fix

--- src/components/UI/product-item.jsx
+++ src/components/UI/product-item.jsx
@@ -49,19 +49,23 @@ const ProductItem = ({
               alt={alt}
             />
           </CardActionArea>
-          <CardContent sx={{ pl: 0 }}>
+          <CardContent sx={{ pl: 0, pr: 0}}>
             <p className='font-Plus-Jakarta-Sans text-base lg:text-xl text-slate-700 pb-2 uppercase'>
               {name}
             </p>
-            <div className='flex justify-between gap-5'>
+            <div className='flex justify-between gap-6'>
+              <>
               <h6 className='my-auto font-Plus-Jakarta-Sans text-base font-semibold lg:text-xl text-slate-700'>
                 ₦{Number(price).toLocaleString()}
               </h6>
+              </>
+              <>
               {stock === 'Out of Stock' && (
                 <p className='my-auto font-Plus-Jakarta-Sans text-sm lg:text-xl text-red-700 pb-2 uppercase'>
                   {stock}
                 </p>
-              )}
+                )}
+              </>
             </div>
           </CardContent>
         </Link>
