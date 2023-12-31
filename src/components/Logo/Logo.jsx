73afdc51276d913a11adb2c50aebad4bccc8d0e4
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-12-31 16:05:10 +0100

    fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,17 @@
+import { Typography } from "@mui/material";
 import Link from "next/link";
-import Typography from "../Fonts/Typography";
 
 const Logo = () => {
   return (
     <>
-      <Link href="/" className="text-gray-700 grid">
-        <Typography variant="h2">ROYESHOES</Typography>
+      <Link href='/' className='text-gray-700 grid'>
+        <Typography variant='h4'
+          sx={{ textTransform: 'uppercase' }}>
+          Storex
+        </Typography>
       </Link>
     </>
-  );
+  )
 };
 
 export default Logo;
