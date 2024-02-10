00ccd3017e02347f03d29330495e33cfed6e7466
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-02-10 08:52:30 +0100

    fix

--- src/components/UI/product-single.jsx
+++ src/components/UI/product-single.jsx
@@ -57,8 +57,8 @@ const ProductSingle = () => {
   const handleAddToCart = () => {
     if (
       !isAdded &&
-      selectedSize.length === 0 &&
-      selectedColor.length === 0 &&
+      selectedSize.length > 1 &&
+      selectedColor.length > 1 &&
       !selectedSize &&
       !selectedColor
     ) {
