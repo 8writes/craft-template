e70f2be83a7f0e5d6f742d3dbb0a76e66f641e46
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-03-19 18:26:24 +0100

    fix

--- src/containers/common/Footer.jsx
+++ src/containers/common/Footer.jsx
@@ -2,12 +2,22 @@
 
 import Link from 'next/link'
 import { Typography } from '@/components'
+import { useState, useEffect } from 'react'
 
 const Footer = () => {
+   const [subdomain, setSubdomain] = useState('')
+
+   useEffect(() => {
+     const subdomain = window.location.hostname.split('.')[0]
+     setSubdomain(subdomain)
+   }, [])
+  
   return (
     <>
       <section className='flex justify-center py-16 bg-slate-100 text-gray-700'>
-        <Typography variant='p.medium'>Copyright © 2023 - 2024.</Typography>
+        <Typography variant='p.medium'>
+          © {subdomain} 2024.
+        </Typography>
         <Typography variant='p.medium'>
           &nbsp; powered by <Link href='https://craaft.com.ng'>craaft.</Link>{' '}
         </Typography>
